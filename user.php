021470007cf6a10ebed79f51a10a749d9a683efa
Pooja Kumari <76776825+Poojakumari123@users.noreply.github.com>
2021-09-26 11:31:33 +0530

    Update user.php

--- user.php
+++ user.php
@@ -7,15 +7,16 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  
   <link rel="stylesheet" type="text/css" href="css/table.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
   <style type="text/css">
+    .container-fluid {
+      font-size: 30px;
+    }
     button{
       transition: 1.5s;
       background-color : rgb(71, 168, 175);
@@ -31,9 +32,9 @@
     <title>AllUsers</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color:rgb(71, 168, 175)">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:rgb(98, 196, 196);">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#" style="font-size: 32px; padding-right: 30px;">Home</a>
+      <a class="navbar-brand" href="index.html" style="font-size: 32px; padding-right: 30px;">Home</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -50,7 +51,7 @@
         <div class="Foundation" aria-current="page" href="#"><u> Foundation Bank</u></div>
       </div>
     </div>
-  </nav>
+</nav>
 
 <div class= "container">
     <h2 class="text-center pt-4" >Transfer Money</h2>
@@ -98,13 +99,5 @@
   </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
-    integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT"
-    crossorigin="anonymous"></script>
- -->
 </body>
 </html>
